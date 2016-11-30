3987fc8abd5c1cb0bb445929c6f5a9b66c48c14e
yuanyunchao <yuanyunchao@fenergy.com.cn>
2016-11-30 18:05:30 +0800

    test

--- tools/http_test.py
+++ tools/http_test.py
@@ -3,16 +3,22 @@ import httplib,urllib,random,json,string ;
 
 class ecs_http_client:
     host = "127.0.0.1:8000"
-    def list_command(self,host = ecs_http_client.host):
+    def list_command(self,host = None):
+        if host == None :
+            host = self.host ; 
         resp = requests.get(host+"/station/list_command/");
         return json.loads(resp.text) 
 
-    def insert_command(self,host=ecs_http_client.host,key,code):
+    def insert_command(self,host=None,key,code):
+        if host == None :
+            host = self.host ; 
         resp = requests.get(host+"/station/insert_command/",data = json.dumps({"key":str(key),"code":str(code)}));
         return json.loads(resp.text)
     
-    def delete_command(self,host=ecs_http_client.host,key=""):
-        if len(key) > 0 :
+    def delete_command(self,host=None,key=None):
+        if host == None :
+            host = self.host ; 
+        if key == None :
             resp = requests.get(host+"/station/delete_command/",data=json.dumps([str(key)]));
         else :
             resp = requests.get(host+"/station/delete_command/");
